1125f91e7a655d08e965de9e38c0ab07886e1893
Lorkiyn <Lorkiyn@users.noreply.github.com>
2016-04-09 21:36:33 +0200

    Changes

--- src/Fußball/Programm.java
+++ src/Fußball/Programm.java
@@ -1,9 +1,9 @@
-package Fuﬂball;
+package Fu√üball;
 
 public class Programm {
 
 	public static void main(String[] args) {
-		Spieler s = new Spieler("Alex", 17, 10, 22, 10, 22);
+		Spieler s = new Spieler("Kevin", 17, 10, 22, 10, 22);
 		Trainer t = new Trainer("Darian", 17, 10);
 		
 		System.out.println(s +"\n" +t);
