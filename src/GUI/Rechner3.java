92307080caf97455af187b0fe53e2bc6c0ebe26a
Lorkiyn <dari18@online.de>
2016-04-13 23:13:03 +0200

    Added features

--- src/GUI/Rechner3.java
+++ src/GUI/Rechner3.java
@@ -3,6 +3,7 @@ package GUI;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -12,6 +13,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.ActionEvent;
@@ -19,21 +21,24 @@ import java.awt.event.ActionEvent;
 public class Rechner3 extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JLabel zahl1Label;
-	private JLabel zahl2Label;
-	private JTextField zahl1;
-	private JTextField zahl2;
+	private JLabel number1Label;
+	private JLabel number2Label;
+	private JTextField number1JText;
+	private JTextField number2JText;
 	private JButton add;
-	private JTextField ergebnis;
-	private JButton neu;
-	private JButton exit;
+	private JTextArea resultJText;
+	private JButton newJButton;
+	private JButton exitJButton;
 	private JButton mult;
 	private JButton sub;
 	private JButton div;
+	private JButton sqrt;
+	private JButton pow2;
+	private JButton ans;
+	private JButton powX;
+	private double ansResult = 0;
+	private JLabel lblNewLabel;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -47,218 +52,282 @@ public class Rechner3 extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public Rechner3() {
+
 		setAlwaysOnTop(true);
 		setTitle("Taschenrechner");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 330, 210);
+		setBounds(100, 100, 400, 217);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		zahl1Label = new JLabel("Zahl 1");
-		zahl1Label.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		zahl1Label.setHorizontalAlignment(SwingConstants.CENTER);
-		zahl1Label.setBounds(10, 11, 70, 30);
-		contentPane.add(zahl1Label);
+		number1Label = new JLabel("Zahl 1");
+		number1Label.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		number1Label.setHorizontalAlignment(SwingConstants.CENTER);
+		number1Label.setBounds(0, 9, 60, 30);
+		contentPane.add(number1Label);
 
-		zahl2Label = new JLabel("Zahl 2");
-		zahl2Label.setHorizontalAlignment(SwingConstants.CENTER);
-		zahl2Label.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		zahl2Label.setBounds(10, 52, 70, 30);
-		contentPane.add(zahl2Label);
+		number2Label = new JLabel("Zahl 2");
+		number2Label.setHorizontalAlignment(SwingConstants.CENTER);
+		number2Label.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		number2Label.setBounds(0, 50, 60, 30);
+		contentPane.add(number2Label);
 
 		//Zahl 1 ---------------------------------------------------
-		zahl1 = new JTextField();
-        addKeyListener(new KeyAdapter() {
-            public void keyTyped(KeyEvent e) {
-                if (!Character.isDigit(e.getKeyChar()))
-                    e.consume();
-            }
-        });
-		zahl1.addMouseListener(new MouseListener() {
+		number1JText = new JTextField();
+		number1JText.addKeyListener(new KeyListener() {
+		    public void keyPressed(KeyEvent e) {
+		        if(e.getKeyCode() == 10) {
+		        	number2JText.requestFocus();
+		        }
+		    }
+		    public void keyReleased(KeyEvent e) {}
+		    public void keyTyped(KeyEvent e) {}
+		});
+		number1JText.addMouseListener(new MouseListener() {
 			public void mouseClicked(MouseEvent e) {}
-
-			@Override
 			public void mouseEntered(MouseEvent arg0) {
-				if(zahl1.getText().equals("0")) {
-					zahl1.setText("");
+				if(number1JText.getText().equals("0")) {
+					number1JText.setText("");
 				}
 			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				if(zahl1.getText().equals("")) {
-					zahl1.setText("0");
-				}
-			}
-
-			@Override
+			public void mouseExited(MouseEvent arg0) {}
 			public void mousePressed(MouseEvent arg0) {}
-
-			@Override
 			public void mouseReleased(MouseEvent arg0) {}
 		});
-		zahl1.addActionListener(new ActionListener() {
+		number1JText.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		zahl1.setBounds(90, 11, 86, 30);
-		contentPane.add(zahl1);
-		zahl1.setColumns(10);
+		number1JText.setBounds(70, 11, 96, 30);
+		contentPane.add(number1JText);
+		number1JText.setColumns(10);
 
 		//Zahl2 ----------------------------------------------------
-		zahl2 = new JTextField();
-		zahl2.addMouseListener(new MouseListener() {
+		number2JText = new JTextField();
+		number2JText.addMouseListener(new MouseListener() {
 			public void mouseClicked(MouseEvent e) {}
-
-			@Override
 			public void mouseEntered(MouseEvent arg0) {
-				if(zahl2.getText().equals("0")) {
-					zahl2.setText("");
+				if(number2JText.getText().equals("0")) {
+					number2JText.setText("");
 				}
 			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				if(zahl2.getText().equals("")) {
-					zahl2.setText("0");
-				}
-			}
-
-			@Override
+			public void mouseExited(MouseEvent arg0) {}
 			public void mousePressed(MouseEvent arg0) {}
-
-			@Override
 			public void mouseReleased(MouseEvent arg0) {}
 		});
-		zahl2.addActionListener(new ActionListener() {
+		number2JText.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		zahl2.setColumns(10);
-		zahl2.setBounds(90, 52, 86, 30);
-		contentPane.add(zahl2);
+		number2JText.setColumns(10);
+		number2JText.setBounds(70, 52, 96, 30);
+		contentPane.add(number2JText);
 
 		//Addieren --------------------------------------------------
 		add = new JButton("+");
+		add.setToolTipText("Addieren der Zahlen 1 und 2");
 		add.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				setFields();
-				double dErgebnis = Double.parseDouble(zahl1.getText().trim().replace(',', '.')) + Double.parseDouble(zahl2.getText().trim().replace(',', '.'));
-				setErgebnis(dErgebnis);
+				try {
+					double result = Double.parseDouble(number1JText.getText().trim().replace(',', '.')) + Double.parseDouble(number2JText.getText().trim().replace(',', '.'));
+					setResult(result);
+				} catch (NumberFormatException e) {
+					resultJText.setText("Es sind nur nummerische     Werte erlaubt.");
+				}
 			}
 		});
-		add.setBounds(186, 11, 50, 30);
+		add.setBounds(176, 11, 50, 30);
 		contentPane.add(add);
 
 		//Subtrahieren ----------------------------------------------
 		sub = new JButton("-");
+		sub.setToolTipText("Subtrahieren der Zahlen 1 und 2");
 		sub.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				setFields();
-				double dErgebnis = Double.parseDouble(zahl1.getText().trim().replace(',', '.')) - Double.parseDouble(zahl2.getText().trim().replace(',', '.'));
-				setErgebnis(dErgebnis);
+				try {
+					double result = Double.parseDouble(number1JText.getText().trim().replace(',', '.')) - Double.parseDouble(number2JText.getText().trim().replace(',', '.'));
+					setResult(result);
+				} catch (NumberFormatException e) {
+					resultJText.setText("Es sind nur nummerische     Werte erlaubt.");
+				}
 			}
 		});
-		sub.setBounds(246, 11, 50, 30);
+		sub.setBounds(236, 11, 50, 30);
 		contentPane.add(sub);
 
 		//Multiplizieren --------------------------------------------
 		mult = new JButton("*");
+		mult.setToolTipText("Multiplizieren der Zahlen 1 und 2");
 		mult.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				setFields();
-				double dErgebnis = Double.parseDouble(zahl1.getText().trim().replace(',', '.')) * Double.parseDouble(zahl2.getText().trim().replace(',', '.'));
-				setErgebnis(dErgebnis);
+				try {
+					double result = Double.parseDouble(number1JText.getText().trim().replace(',', '.')) * Double.parseDouble(number2JText.getText().trim().replace(',', '.'));
+					setResult(result);
+				} catch (NumberFormatException e) {
+					resultJText.setText("Es sind nur nummerische     Werte erlaubt.");
+				}
 			}
 		});
-		mult.setBounds(186, 52, 50, 30);
+		mult.setBounds(176, 52, 50, 30);
 		contentPane.add(mult);
 
 		//Dividieren ------------------------------------------------
 		div = new JButton("/");
+		div.setToolTipText("Dividieren der Zahlen 1 und 2");
 		div.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				setFields();
-				double dErgebnis = Double.parseDouble(zahl1.getText().trim().replace(',', '.')) / Double.parseDouble(zahl2.getText().trim().replace(',', '.'));
-				setErgebnis(dErgebnis);
+				try {
+					double result = Double.parseDouble(number1JText.getText().trim().replace(',', '.')) / Double.parseDouble(number2JText.getText().trim().replace(',', '.'));
+					setResult(result);
+				} catch (NumberFormatException e) {
+					resultJText.setText("Es sind nur nummerische     Werte erlaubt.");
+				}
 			}
 		});
-		div.setBounds(246, 52, 50, 30);
+		div.setBounds(236, 52, 50, 30);
 		contentPane.add(div);
 
-		//Ergebnis --------------------------------------------------
-		ergebnis = new JTextField();
-		ergebnis.setHorizontalAlignment(SwingConstants.CENTER);
-		ergebnis.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		ergebnis.setEditable(false);
-		ergebnis.setBounds(20, 93, 156, 72);
-		contentPane.add(ergebnis);
-		ergebnis.setColumns(10);
-
-		//Neu
-		neu = new JButton("Neu");
-		neu.addActionListener(new ActionListener() {
+		//Wurzel ---------------------------------------------------
+		sqrt = new JButton("\u221A");
+		sqrt.setToolTipText("Wurzel aus Zahl1");
+		sqrt.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				zahl1.setText(null);
-				zahl2.setText(null);
-				ergebnis.setText(null);
+				setFields();
+				try {
+					double result = Math.sqrt(Double.parseDouble(number1JText.getText().trim().replace(',', '.')));
+					number2JText.setText("0");
+					setResult(result);
+				} catch (NumberFormatException e) {
+					resultJText.setText("Es sind nur nummerische     Werte erlaubt.");
+				}
 			}
 		});
-		neu.setBounds(186, 93, 110, 30);
-		contentPane.add(neu);
+		sqrt.setBounds(176, 93, 50, 30);
+		contentPane.add(sqrt);
+
+		//Potenzieren ----------------------------------------------
+		pow2 = new JButton("x\u00B2");
+		pow2.setToolTipText("Zahl1 quadrieren");
+		pow2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setFields();
+				try {
+					double result = Math.pow(Double.parseDouble(number1JText.getText().trim().replace(',', '.')), 2);
+					setResult(result);
+				} catch (NumberFormatException e) {
+					resultJText.setText("Es sind nur nummerische     Werte erlaubt.");
+				}
+			}
+		});
+		pow2.setBounds(236, 93, 50, 30);
+		contentPane.add(pow2);
+
+		powX = new JButton("x*");
+		powX.setToolTipText("Quadriert Zahl1 mit dem Wert von Zahl2");
+		powX.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setFields();
+				try {
+					double result = Math.pow(Double.parseDouble(number1JText.getText().trim().replace(',', '.')), Double.parseDouble(number2JText.getText().trim().replace(',', '.')));
+					setResult(result);
+				} catch (NumberFormatException e) {
+					resultJText.setText("Es sind nur nummerische     Werte erlaubt.");
+				}
+			}
+		});
+		powX.setBounds(236, 135, 50, 30);
+		contentPane.add(powX);
+
+		ans = new JButton("Ans");
+		ans.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		ans.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(ansResult > 0) {
+					if(ansResult - (int) ansResult == 0) {
+						number1JText.setText(String.valueOf((int)ansResult));
+						number2JText.setText("");
+					} else {
+						number1JText.setText(String.valueOf(ansResult));
+						number2JText.setText("");
+					}
+				}
+			}
+		});
+		ans.setToolTipText("Enth\u00E4lt das Ergebnis der letzen Rechnung");
+		ans.setBounds(176, 135, 50, 30);
+		contentPane.add(ans);
+
+		//Ergebnis --------------------------------------------------
+		resultJText = new JTextArea();
+		resultJText.setToolTipText("Ergebnisfeld");
+		resultJText.setWrapStyleWord(true);
+		resultJText.setLineWrap(true);
+		resultJText.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		resultJText.setEditable(false);
+		resultJText.setBounds(10, 91, 156, 72);
+		contentPane.add(resultJText);
+		resultJText.setColumns(10);
+
+		//Neu
+		newJButton = new JButton("CE");
+		newJButton.setToolTipText("Neue Rechnung beginnen");
+		newJButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				number1JText.requestFocus();
+				number1JText.setText(null);
+				number2JText.setText(null);
+				resultJText.setText(null);
+			}
+		});
+		newJButton.setBounds(296, 12, 78, 110);
+		contentPane.add(newJButton);
 
 		//Beenden
-		exit = new JButton("Beenden");
-		exit.addActionListener(new ActionListener() {
+		exitJButton = new JButton("X");
+		exitJButton.setToolTipText("Beendet das Program");
+		exitJButton.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		exitJButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
 			}
 		});
-		exit.setBounds(186, 135, 110, 30);
-		contentPane.add(exit);
+		exitJButton.setBounds(296, 135, 78, 30);
+		contentPane.add(exitJButton);
+		
+		lblNewLabel = new JLabel(" Taschenrechner v1.2 by Darian");
+		lblNewLabel.setEnabled(false);
+		lblNewLabel.setFont(new Font("Tahoma", Font.ITALIC, 9));
+		lblNewLabel.setBounds(236, 166, 138, 8);
+		contentPane.add(lblNewLabel);
 	}
 
 	public void setFields() {
-		if(zahl1.getText().equals("")) {
-			zahl1.setText("0");
+		if(number1JText.getText().equals("")) {
+			number1JText.setText("0");
 		}
-		
-		if(zahl2.getText().equals("")) {
-			zahl2.setText("0");
+
+		if(number2JText.getText().equals("")) {
+			number2JText.setText("0");
 		}
 	}
 
-	public void setErgebnis(double number) {
-		String str = zahl1.getText(), str2 = zahl2.getText();
-		double zahl1 = Double.parseDouble(str.trim().replace(',', '.')), zahl2 = Double.parseDouble(str2.trim().replace(',', '.'));
+	public void setResult(double number) {
 		int numberInt = (int) number;
-		String ergebnisInt = String.valueOf(numberInt);
-		String ergebnisDouble = String.valueOf(number);
+		String resultInt = String.valueOf(numberInt);
+		String resultDouble = String.valueOf(number);
+		ansResult = number;
 
-		if(zahl1 - (int)zahl1 == 0 && zahl2 - (int)zahl2 == 0) {
-			ergebnis.setText(ergebnisInt);
+		if(number - (int)number == 0) {
+			resultJText.setText(resultInt);
 		} else {
-			ergebnis.setText(ergebnisDouble);
+			resultJText.setText(resultDouble);
 		}
-	}
-
-	public void enableOperators() {
-		add.setEnabled(true);
-		mult.setEnabled(true);
-		sub.setEnabled(true);
-		div.setEnabled(true);
-	}
-
-	public void disableOperators() {
-		add.setEnabled(false);
-		mult.setEnabled(false);
-		sub.setEnabled(false);
-		div.setEnabled(false);
 	}
 }
